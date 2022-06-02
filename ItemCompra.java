e779cb6695c8a37c0b133872d5cef90fec795c6c
Aline Dório <85902654+alinedorio@users.noreply.github.com>
2022-06-02 13:31:29 -0300

    Add files via upload

---
+++ ItemCompra.java
@@ -0,0 +1,22 @@
+package LojaVirtual;
+
+public class ItemCompra {
+	Produto produto;
+	int quantidade;
+	double custoUnidade;
+	
+	ItemCompra (Produto p, int q, double c){
+		produto = p;
+		quantidade = q;
+		custoUnidade = c;
+	}
+	
+	public String toString() {
+		return "Produto (" + produto + ", qtd = " + quantidade + ", custo = " + custoUnidade + ")";
+	}
+	
+	double custoTotal() {
+		
+		return
+	}
+}
