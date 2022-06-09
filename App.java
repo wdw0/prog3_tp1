cd0464c26a25ce3d3faa907ba4d07953a1b21365
Aline Dório <85902654+alinedorio@users.noreply.github.com>
2022-06-09 11:38:21 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,18 +1,14 @@
 package Loja;
 
 public class App {
-	public static void main(String[] args) {
-		Produto p = new Produto(123, "agua", 12, "bebida", 5, 10);
-		Produto p2 = new Produto(321, "refri", 18, 20);
-		
-		System.out.println(p);
-		
-		p.atualizarEstoque(5);
-		p.atualizarDesconto(2);
-		p.atualizarPreco(10);
-		
-	
-		System.out.println(p);
-	}
+    public static void main(String[] args) {
+        Loja l = new Loja();
+
+        l.cadastrarCliente();
+        l.cadastrarProduto();
+        l.cadastrarNota();
+
+
+
 		
 }
