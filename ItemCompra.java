5e18fe933ab4c2fcb849cda9d25c9a0c47a227ce
Aline Dório <85902654+alinedorio@users.noreply.github.com>
2022-06-09 11:45:03 -0300

    Update ItemCompra.java

--- ItemCompra.java
+++ ItemCompra.java
@@ -1,22 +1,22 @@
 package Loja;
 
 public class ItemCompra {
-	Produto produto;
-	int quantidade;
-	double custoUnidade;
-	
-	ItemCompra (Produto p, int q, double c){
-		produto = p;
-		quantidade = q;
-		custoUnidade = c;
-	}
-	
-	public String toString() {
-		return "Produto (" + produto + ", qtd = " + quantidade + ", custo = " + custoUnidade + ")";
-	}
-	
-	double custoTotal() {
-		
-		return this.quantidade * this.custoUnidade;
-	}
+    Produto produto;
+    int quantidade;
+    double custoUnidade;
+
+    ItemCompra (Produto p, int q){
+        produto = p;
+        quantidade = q;
+        custoUnidade = p.custo();
+    }
+
+    public String toString() {
+        return "Item Comprado (" + produto + ", qtd = " + quantidade + ", custo = " + custoUnidade + ")";
+    }
+
+    double custoTotal() {
+
+        return this.quantidade * this.custoUnidade;
+    }
 }
