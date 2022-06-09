40ba3b8f417636d45027ccf8cc6ad624cdb161d2
Aline Dório <85902654+alinedorio@users.noreply.github.com>
2022-06-09 11:47:28 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,24 +1,24 @@
 package LojaVirtual;
 
 public class Cliente {
-	String cpf;
-	String nome;
-	String telefone;
-	
-	Cliente (String cpf, String nome, String telefone){
-		this.cpf = cpf;
-		this.nome = nome;
-		this.telefone = telefone;
-	}
-	
-	
-	//public Cliente(String cpf) {
-	//	super();
-	//	this.cpf = cpf;
-	//}
+    String cpf;
+    String nome;
+    String telefone;
+
+    Cliente (String cpf, String nome, String telefone){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
 
 
-	public String toString() {
-		return "Cliente(" + nome + ", cpf = " + cpf + ", tel = " + telefone + ")";
-	}
+    //public Cliente(String cpf) {
+    //	super();
+    //	this.cpf = cpf;
+    //}
+
+
+    public String toString() {
+        return "Cliente(" + nome + ", cpf=" + cpf + ", tel=" + telefone + ")";
+    }
 }
