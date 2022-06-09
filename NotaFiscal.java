9daeeecd06fb704b12a2bc95be3e2f509627cbe1
Aline Dório <85902654+alinedorio@users.noreply.github.com>
2022-06-09 11:43:23 -0300

    Update NotaFiscal.java

--- NotaFiscal.java
+++ NotaFiscal.java
@@ -3,33 +3,35 @@ package Loja;
 import java.util.ArrayList;
 
 public class NotaFiscal {
-	int codigo;
-	Cliente comprador;
-	String data;
-	String metodoPagamento;
-	ArrayList<ItemCompra> itens;
-	
-	NotaFiscal (int cod, Cliente com, String mP){
-		codigo = cod;
-		comprador = com;
-		metodoPagamento = mP;
-	}
-	
-	public String toString() {
-		return "Nota Fiscal (" + codigo + ", comprador = " + comprador + ", data = " + data + ", pagamento = " + metodoPagamento +")";
-	}
-	
-	void adicionarItem(ItemCompra item) {
-		this.itens.add(item);
-	}
-	
-	void custoTotal() {
-		double custoTotal = 0;
-		
-		for(ItemCompra item : this.itens)
-		    custoTotal += item.custoTotal();
-		
-		return custoTotal;
-	}
+    int codigo;
+    Cliente comprador;
+    private LocalDate data = LocalDate.now();
+    String metodoPagamento;
+    ArrayList<ItemCompra> itens;
+
+    NotaFiscal (int cod, Cliente com, String mP){
+        codigo = cod;
+        comprador = com;
+        metodoPagamento = mP;
+        itens = new ArrayList<ItemCompra>();
+    }
+
+    public String toString() {
+        return "Nota Fiscal (" + codigo + ", comprador=" + comprador + ", data=" + data + ", pagamento=" + metodoPagamento +")";
+    }
+
+    void adicionarItem(ItemCompra item) {
+        this.itens.add(item);
+    }
+
+    double custoTotal() {
+
+        double custo = 0;
+
+        for(ItemCompra item : this.itens)
+            custo += item.custoTotal();
+
+        return custo;
+    }
 	
 }
