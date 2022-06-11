6d370def8a60bc3e6b7e5c7c212919b737d4d67a
Aline Dório <85902654+alinedorio@users.noreply.github.com>
2022-06-11 15:15:31 -0300

    Add files via upload

---
+++ Loja/Produto.java
@@ -0,0 +1,71 @@
+package Loja;
+
+public class Produto {
+    int codigo;
+    String nome;
+    double preco;
+    String categoria;
+    double desconto;
+    int estoque;
+
+    public Produto(int codigo, String nome, double preco, String categoria, double desconto, int estoque) {
+        super();
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.desconto = desconto;
+        this.estoque = estoque;
+    }
+
+    public Produto(int codigo, String nome, double preco, int estoque) {
+        super();
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    double custo() {
+        double descCalculado = preco * desconto;
+        return preco - descCalculado;
+    }
+
+    void atualizarEstoque(int qtd) {
+        if(qtd >= 0) {
+            estoque = qtd;
+        }
+        else {
+            System.out.println("Não é possível registrar um estoque negativo");
+        }
+    }
+    void atualizarDesconto(double v) {
+        if(v >= 0) {
+            desconto = v;
+        }
+        else {
+            System.out.println("Não é possível aplicar um desconto negativo");
+        }
+    }
+    void atualizarPreco(double v) {
+        if(v >= 0) {
+            preco = v;
+        }
+        else {
+            System.out.println("Não é possível definir um preço negativo ou igual a zero");
+        }
+    }
+
+    public int getCod(){
+        return codigo;
+    }
+
+    public String toString() {
+        return "Produto [codigo=" + codigo + ", nome=" + nome + ", preco=" + preco + ", categoria=" + categoria
+                + ", desconto=" + desconto + ", estoque=" + estoque + "]";
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+}
